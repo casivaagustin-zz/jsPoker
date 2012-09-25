0f187e04e2f824d1bddc7b97406668e30be4491e
Agustin Casiva <casivaagustin@gmail.com>
2012-09-25 20:56:01 -0300

    Tests improvements

---
+++ js/application_tests.js
@@ -0,0 +1,15 @@
+/*
+ * Poker Game Register for require.js
+ */
+require(["vendor/jquery/jquery"], function(lib) { });
+require(["vendor/qunit/qunit"], function(lib) { });
+
+require(["poker/core/Card"], function(lib) { });
+require(["poker/core/Deck"], function(lib) { });
+require(["poker/core/Hand"], function(lib) { });
+require(["poker/core/Player"], function(lib) { });
+require(["poker/core/Exceptions"], function(lib) { });
+require(["poker/core/PokerGame"], function(lib) { });
+//require(["poker/ui/Ui"], function(lib) { });
+//require(["poker/Poker"], function(lib) { });
+require(["poker/tests/tests"], function(lib) { });
