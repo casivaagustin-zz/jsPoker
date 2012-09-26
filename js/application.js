1bb02dcd865a5855a5420ae6c7379248dd814a4d
Agustin Casiva <casivaagustin@gmail.com>
2012-09-25 21:29:06 -0300

    Require Js sync

--- js/application.js
+++ js/application.js
@@ -1,14 +1,16 @@
 /*
  * Poker Game Register for require.js
  */
-require(["vendor/jquery/jquery"], function(lib) { });
-require(["vendor/bootstrap/bootstrap"], function(lib) { });
-
-require(["poker/core/Card"], function(lib) { });
-require(["poker/core/Deck"], function(lib) { });
-require(["poker/core/Hand"], function(lib) { });
-require(["poker/core/Player"], function(lib) { });
-require(["poker/core/Exceptions"], function(lib) { });
-require(["poker/core/PokerGame"], function(lib) { });
-require(["poker/ui/Ui"], function(lib) { });
-require(["poker/Poker"], function(lib) { });
+require([
+        "vendor/jquery/jquery", 
+        "vendor/bootstrap/bootstrap", 
+        "poker/core/Card", 
+        "poker/core/Deck", 
+        "poker/core/Hand", 
+        "poker/core/Player", 
+        "poker/core/Exceptions", 
+        "poker/core/PokerGame", 
+        "poker/ui/Ui", 
+        "poker/Poker"], 
+        function (libs) { }
+); 
--- js/application_tests.js
+++ js/application_tests.js
@@ -1,15 +1,15 @@
 /*
  * Poker Game Register for require.js
  */
-require(["vendor/jquery/jquery"], function(lib) { });
-require(["vendor/qunit/qunit"], function(lib) { });
-
-require(["poker/core/Card"], function(lib) { });
-require(["poker/core/Deck"], function(lib) { });
-require(["poker/core/Hand"], function(lib) { });
-require(["poker/core/Player"], function(lib) { });
-require(["poker/core/Exceptions"], function(lib) { });
-require(["poker/core/PokerGame"], function(lib) { });
-//require(["poker/ui/Ui"], function(lib) { });
-//require(["poker/Poker"], function(lib) { });
-require(["poker/tests/tests"], function(lib) { });
+require([
+        "vendor/jquery/jquery", 
+        "vendor/qunit/qunit", 
+        "poker/core/Card", 
+        "poker/core/Deck", 
+        "poker/core/Hand", 
+        "poker/core/Player", 
+        "poker/core/Exceptions", 
+        "poker/core/PokerGame", 
+        "poker/tests/tests"], 
+        function (libs) { }
+); 
